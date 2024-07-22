bde85321dfe7697efc0ed1eb1593894074c66b44
PurviSolanki18 <purvisolanki@fincoopers.in>
2024-07-22 17:14:20 +0530

    fisrt

---
+++ app/home/page.js
@@ -0,0 +1,14 @@
+
+
+import Header from '../../components/Header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <p>Content of the home page</p>
+    </div>
+  );
+}
+
+export default HomePage;
